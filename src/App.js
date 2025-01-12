3253c7f0b16dbd00bfd121b78b78cb5c450e65ad
Mateusz Polański <mp.mateusz.polanski@gmail.com>
2025-01-12 11:29:58 +0100

    Add movie list

--- src/App.js
+++ src/App.js
@@ -3,34 +3,21 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const initialMovie = "Memento"
-  const [title, setTitle] = useState(initialMovie)
 
-  function handleChange(event) {
-    setTitle(event.target.value)
-  }
-
-  function getTitleLengthText(length) {
-    if (length == 0) {
-      return null
-    } else if (length < 5) {
-      return `A little too short: ${length} characters.`
-    } else if (length <= 20) {
-      return `Seems to be perfect: ${length} characters.`
-    } else {
-      return `That's too long: ${length} characters.`
-    }
-  }
-
-  let titleLengthText = getTitleLengthText(title.length)
+  const movies = [
+    {title: "Wall-E"},
+    {title: "Pulp Fiction"},
+    {title: "Matrix"},
+    {title: "1670"},
+  ];
 
   return (
     <div className='App-header'>
       <h1>My favorite movies to watch</h1>
-      <h2>My favorite movie for today is {title}.</h2>
-      <h3>{titleLengthText}</h3>
-      <input type="text" value={title} onChange={handleChange}/>
-      {title.length > 0 && <button onClick={() => alert(title)}>Show title</button>}
+      <h2>Titles</h2>
+      <ul>
+        {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+      </ul>
       <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
