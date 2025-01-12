03379969efc01c595a4edfb00ac9196965cc3743
Mateusz Polański <mp.mateusz.polanski@gmail.com>
2025-01-12 10:09:39 +0100

    Add basic layout with two text fields and one input

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let title = "Memento"
+
+  function handleChange(event) {
+    console.log(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-header'>
+      <h1>My favorite movies to watch</h1>
+      <h2>My favorite movie for today is {title}.</h2>
+      <input type="text" onChange={handleChange}/>
     </div>
   );
 }
