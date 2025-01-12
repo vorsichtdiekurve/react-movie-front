3c5b4f14243277edadc31e66d27a2bfc728b68b3
Mateusz Polański <mp.mateusz.polanski@gmail.com>
2025-01-12 10:39:36 +0100

    Add logical condition

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  let title = "Memento"
+  const initialMovie = "Memento"
+  const [title, setTitle] = useState(initialMovie)
+  const [titleLengthText, setTitleLengthText] = useState(getTitleLengthText(initialMovie.length))
 
   function handleChange(event) {
-    console.log(event.target.value)
+    setTitle(event.target.value)
+    setTitleLengthText(getTitleLengthText(event.target.value.length))
+  }
+
+  function getTitleLengthText(length) {
+    if (length == 0) {
+      return null
+    } else if (length < 5) {
+      return `A little too short: ${length} characters.`
+    } else if (length <= 20) {
+      return `Seems to be perfect: ${length} characters.`
+    } else {
+      return `That's too long: ${length} characters.`
+    }
   }
 
   return (
     <div className='App-header'>
       <h1>My favorite movies to watch</h1>
       <h2>My favorite movie for today is {title}.</h2>
-      <input type="text" onChange={handleChange}/>
+      <h3>{titleLengthText}</h3>
+      <input type="text" value={title} onChange={handleChange} />
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
